9b0086ee10f40c933b7806045e0ab6068307f81e
Sergio Santos <ssantos@signo-net.com>
2011-04-30 20:06:34 +0200

    Explicit migration order

---
+++ db/migrate/002_translate_chuck_norris_facts.rb
@@ -0,0 +1,30 @@
+# Encoding: UTF-8
+# Written by: Signo-Net
+# Email: clientes@signo-net.com 
+# Web: http://www.signo-net.com 
+
+# This work is licensed under a Creative Commons Attribution 3.0 License.
+# [ http://creativecommons.org/licenses/by/3.0/ ]
+
+# This means you may use it for any purpose, and make any changes you like.
+# All we ask is that you include a link back to our page in your credits.
+
+# Looking forward your comments and suggestions! clientes@signo-net.com
+
+class TranslateChuckNorrisFacts < ActiveRecord::Migration
+  def self.up
+    
+    add_column :chuck_norris_facts, :lang, :string, :default => "en"    
+    ChuckNorrisFact.reset_column_information
+    
+    factsEs = IO.readlines(File.dirname(__FILE__) + "/facts_es.txt");
+    factsEs.collect {|fact| 
+      ChuckNorrisFact.create(:description => fact, :lang => "es")
+    }    
+    
+  end
+
+  def self.down
+    remove_column :chuck_norris_facts, :lang
+  end
+end
